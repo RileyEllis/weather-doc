ee0778b3ac9ef43f13fd5f09cb4140a84e71ccd4
Riley Ellis <swimguy4914@gmail.com>
2019-04-07 22:32:21 -0500

    card update

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,15 @@ class Card extends Component {
     render() {
         return (
             <div className="weather-card">
-                <p>Test</p>
+                <div className="weather-card-section">
+                    <h1>Time</h1>
+                </div>
+                <div className="weather-card-section">
+                    <h1>Icon</h1>
+                </div>
+                <div className="weather-card-section">
+                    <h1>Info</h1>
+                </div>
             </div>
         );
     }
